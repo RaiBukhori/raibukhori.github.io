2d3ea06fa0e8f0a92dafbcd8e1f09962269568ea
RaiBukhori <raiibukhori@gmail.com>
2025-06-30 17:55:59 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// service-worker.js
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).then((response) => {
+        const newHeaders = new Headers(response.headers);
+        newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+        newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: newHeaders,
+        });
+      })
+    );
+  }
+});
